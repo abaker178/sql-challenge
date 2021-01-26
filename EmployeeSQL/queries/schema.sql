ce130abb7cebf7fc78fbef61348eafb35e9955d7
Andrew Baker <abaker178@gmail.com>
2021-01-26 11:27:09 -0500

    table schema is complete || created file for analysis

--- EmployeeSQL/queries/schema.sql
+++ EmployeeSQL/queries/schema.sql
@@ -1,29 +1,63 @@
-/*
-DEPARTMENTS
-*/
-DROP TABLE IF EXISTS departments;
-
-/*
-DEPARTMENTS - EMPLOYEES
-*/
-DROP TABLE IF EXISTS dept_emp;
-
-/*
-DEPARTMENTS - MANAGER
-*/
-DROP TABLE IF EXISTS dept_manager;
-
-/*
-EMPLOYEES
-*/
-DROP TABLE IF EXISTS employees;
-
-/*
-DEPARTMENTS - SALARIES
-*/
+/*********
+HARD RESET
+*********/
+-- Drop tables in reverse order to prevent dependancy issues
 DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+/*********
+HARD RESET
+*********/
 
 /*
-DEPARTMENTS - titles
+Table creation
+DEPARTMENTS
+TITLES
+EMPLOYEES
+DEPARTMENTS & EMPLOYEES (JUNCTION)
+DEPARTMENTS & MANAGER (JUNCTION)
+SALARIES
 */
-DROP TABLE IF EXISTS titles;
+CREATE TABLE departments (
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
